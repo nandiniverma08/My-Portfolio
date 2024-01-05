c92a2cde4bddcd7ee44601d10d826942518773ee
nandiniverma08 <vermanandini818@gmail.com>
2024-01-05 16:49:27 +0530

    portfolio header, hero , about section done

---
+++ src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="curson icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>I am a frontend developer</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>I am a backend developer</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="curson icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>I am a Design developer</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
