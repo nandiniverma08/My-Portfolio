c7d89338441e154fc44c40e65149ed49db2972f3
nandiniverma08 <vermanandini818@gmail.com>
2024-01-24 17:41:55 +0530

    Made experience section responsive and new module initialised Projects

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Projects = () => {
+  return (
+    <section>
+      <h2>Projects</h2>
+      {/* <div>
+        {Projects.map((project, id) => {
+          return <div key={id}></div>;
+        })}
+      </div> */}
+    </section>
+  );
+};
