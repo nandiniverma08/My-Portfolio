b492be623561af7e83ffa43fa1c1092398e4d740
nandiniverma08 <vermanandini818@gmail.com>
2024-01-25 13:43:11 +0530

    Project section done with responsiveness

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { ProjectCard } from "./ProjectCard";
+import projects from "../../data/projects.json";
+import styles from "./Projects.module.css";
 
 export const Projects = () => {
   return (
-    <section>
-      <h2>Projects</h2>
-      {/* <div>
-        {Projects.map((project, id) => {
-          return <div key={id}></div>;
+    <section className={styles.container}>
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project} />;
         })}
-      </div> */}
+      </div>
     </section>
   );
 };
